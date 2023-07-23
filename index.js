dfbfb21987c3936f43ab773a4961848e8ac43937
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2023-07-23 18:34:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount( ){
+   hideMessage();
+    if (billAmount.value > 0) {
+       if (cashGiven.value >= billAmount.value){
+          const amountToBeReturned = cashGiven.value - billAmount.value;
+          calculateChange(amountToBeReturned);
+       } else {
+        showMessage("The cash should atleast be equal to the bill amount");
+       } 
+       
+    }else {
+       showMessage("Invalid Bill Amount");
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+   for(let i = 0; i < availableNotes.length; i++){
+     const numberOfNotes = Math.trunc(
+        amountToBeReturned / availableNotes[i]
+        );
+     amountToBeReturned = amountToBeReturned % availableNotes[i];
+     noOfNotes[i].innerText = numberOfNotes;
+   }
+}
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+}
